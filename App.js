aed36a58a7cdcc73a30ccfa1cd57fd1e20658870
unknown <5251sun@gmail.com>
2021-08-16 06:55:42 +0900

    #1-2

--- App.js
+++ App.js
@@ -3,10 +3,18 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-contacts";
 import { Alert } from "react-native";
+import axios from "axios";
+
+const API_KEY = "a769d9553ed37ee11d7be11a8be8eacb";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
   };
   getLocation = async () => {
     try {
@@ -14,6 +22,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
